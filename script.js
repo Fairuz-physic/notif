03b5713925e4931b19878fc7bb7335ac9b9ec0fa
Fairuz-physic <fairuzataya1945@gmail.com>
2024-11-12 11:07:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let kotak = document.getElementById("kotak")
+
+// Step 1: Minta izin untuk menampilkan notifikasi
+if (Notification.permission !== "granted") {
+    Notification.requestPermission();
+}
+
+// Fungsi untuk menampilkan notifikasi
+function showNotification() {
+    if (Notification.permission === "granted") {
+        new Notification("Hitungan selesai!", {
+            body: "Perulangan telah mencapai angka 10",
+            icon: "image/grafikk.png" // sesuaikan dengan icon Anda
+        });
+        
+    }
+}
+
+// Fungsi perulangan
+function startCounting() {
+    let count = 0;
+    let interval = setInterval(() => {
+        count++;
+        kotak.innerHTML = count
+        console.log(count);
+        if (count === 10) {
+            showNotification();
+            clearInterval(interval); // hentikan perulangan setelah mencapai 10
+            window.location.href = window.location.href;
+        }
+    }, 1000); // interval 1 detik
+}
+
+// Panggil fungsi untuk memulai perhitungan
+startCounting();
